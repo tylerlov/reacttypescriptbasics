1087e3827708586914bad7a1e1806b8a76f701a7
stylotylo <tylerlovell@gmail.com>
2022-08-12 18:02:23 -0400

    Defined types for props and children

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {ReactElement, ReactNode} from 'react';
 import './App.css';
 
+// Conventional props
+function Heading({ title }: { title: string}){
+  return <h1>{title}</h1>
+}
+//Using ReactNode as type for children to pass through more than string, say JSX or more 
+function HeadingWithContent({ children }: { children: ReactNode }): ReactElement{
+  return <h1>{children}</h1>
+}
+
+//Default Props
+const defaultContainerProps = {
+  heading: <strong> My Heading </strong>
+}
+//merging two types together, adding on the types defined in the constant above
+function Container ({ heading, children }: { children: ReactNode } & typeof defaultContainerProps): ReactElement{
+  return <div><h1>{heading}</h1>{children}</div>
+}
+Container.defaultProps = defaultContainerProps;
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Heading title="Hello World" />
+      <HeadingWithContent>Hello World 2</HeadingWithContent>
+      <Container>World</Container>
     </div>
   );
 }
